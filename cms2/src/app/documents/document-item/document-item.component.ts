7ac70625612baac432feda4af8c06d31154d56a2
rdschein <robschein17@gmail.com>
2017-06-14 20:47:12 -0600

    L04

--- cms2/src/app/documents/document-item/document-item.component.ts
+++ cms2/src/app/documents/document-item/document-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-item',
@@ -6,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-item.component.css']
 })
 export class DocumentItemComponent implements OnInit {
-
+  @Input() document: Document;
+  @Output() documentSelected = new EventEmitter<void>();
   constructor() { }
 
+  onSelected() {
+    this.documentSelected.emit();
+  }
   ngOnInit() {
   }
 
--- cms2/src/app/documents/document-list/document-list.component.ts
+++ cms2/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[]= [
+    new Document(1, 'Doc1', 'I am document 1', 'http://www.espn.com/', null),
+    new Document(2, 'Doc2', 'I am document 2', 'http://www.espn.com/', null),
+    new Document(3, 'Doc3', 'I am document 3', 'http://www.espn.com/', null),
+    new Document(4, 'Doc4', 'I am document 4', 'http://www.espn.com/', null),
 
+  ]
   constructor() { }
 
   ngOnInit() {
+  }
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
